9d9d47ce38cc1a3c87061170e220886898a8e8af
Francisco Arce <fjarce.g@gmail.com>
2023-01-18 11:17:20 -0300

    Updates

--- src/utilities/format.ts
+++
@@ -1,8 +0,0 @@
-/**
- * 
- * @param {number} value un valor númerico para formatear 
- * @returns {string} el valor con formato de moneda, procesado por una regexp 
- */
-export function formatValue (value: number): string{
-    return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
-  }
